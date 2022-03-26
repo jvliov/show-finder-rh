189232b10865995e625a06ea9e9016b417f74559
Zack Umar <zackumar2@gmail.com>
2022-03-26 18:52:11 -0500

    Some home stuff

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,25 +1,41 @@
-import React, {useStates, useEffect} from 'react'
+import React, { useStates, useEffect } from 'react'
 import * as shows from '../services/show-api'
-import * as styles from './home.module.css'
-
+import './home.css'
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {show: shows.getShow(1000)};
+    this.state = { show: shows.getShow(1000) };
   }
 
-  
-  
+
 
   render() {
 
-    console.log("test",this.state.show)
 
     return (
       <main>
-      <img src={`https://image.tmdb.org/t/p/w500${this.state.show.poster_path}`} alt="Detective Pikachu"></img>
-    </main>
+        <div className="header">
+          <a href='/watchlist'><i className="fa-solid fa-book"></i></a>
+          <h1>ShowFinder</h1>
+          <a href='/settings'><i className="fa-solid fa-gear"></i></a>
+        </div>
+
+        <div className="imageBox">
+
+          <img src={`https://image.tmdb.org/t/p/w500${this.state.show.poster_path}`} alt="Detective Pikachu"></img>
+          <div className='names'>
+            <h2>Name</h2>
+            <h3>genre</h3>
+          </div>
+
+        </div>
+
+        <div className="buttonBox">
+          <button><i className="fa-solid fa-x"></i></button>
+          <button><i className="fa-solid fa-check"></i></button>
+        </div>
+      </main>
     );
   }
 }
