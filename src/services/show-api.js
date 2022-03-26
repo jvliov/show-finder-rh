0c9a1c7f37759504dd946017f7d66edb0add7a57
Zach True <zacharytrue13@gmail.com>
2022-03-26 17:21:37 -0500

    added getShow

--- src/services/show-api.js
+++ src/services/show-api.js
@@ -1,6 +1,6 @@
-import keys from './keys'
+import {showKeys} from './config'
+import axios from 'axios'
 
-keys
 
 showList = undefined
 
@@ -12,9 +12,10 @@ function getList(updateList = false) {
     return showList
 }
 
-function getShow(id) {
+export async function getShow(id) {
     //axios id
-    //return response.data
+    let resp = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${showKeys.tmdb}`)
 
+    //return resp.data
     return {"adult":false,"backdrop_path":"/nDP33LmQwNsnPv29GQazz59HjJI.jpg","genre_ids":[28,12,14],"id":447404,"original_language":"en","original_title":"Pokémon Detective Pikachu","overview":"In a world where people collect pocket-size monsters (Pokémon) to do battle, a boy comes across an intelligent monster who seeks to be a detective.","popularity":123.376,"poster_path":"/wgQ7APnFpf1TuviKHXeEe3KnsTV.jpg","release_date":"2019-05-03","title":"Pokémon Detective Pikachu","video":false,"vote_average":7,"vote_count":5505}  
 }
