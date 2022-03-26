5da2c17314081f538c8c72e92d7c46bc2f090f20
Zach True <zacharytrue13@gmail.com>
2022-03-26 15:32:24 -0500

    added watchMode and tmdb api keys

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,4 @@
-const keys = {
-    key: "test"
+export const showKeys = {
+    watchmode: 'dcgqyFNE9IxAu4bcLXuFabS247LBYv0rD5oFwdmO',
+    tmdb: '8ce054ee2c39e3ba1958dde91b33a4ca'
 }
-
-export default keys
